6f4a3e797ec079e8eb8ceb12ab084c16fc8c0f84
russabiswas <biswasrussa@gmail.com>
2023-11-19 00:42:23 +0100

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -24,12 +24,12 @@ window.addEventListener('DOMContentLoaded', event => {
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
-    //  Activate Bootstrap scrollspy on the main nav element
+    // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
-            rootMargin: '0px 0px -40%',
+            offset: 74,
         });
     };
 
